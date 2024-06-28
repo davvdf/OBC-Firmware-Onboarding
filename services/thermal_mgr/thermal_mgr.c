9fab27c688584e9ce5773fe0fb02a932b4a78c9c
davvdf <76758697+davvdf@users.noreply.github.com>
2024-06-27 20:49:50 -0400

    Made requested changes

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -2,6 +2,7 @@
 #include "errors.h"
 #include "lm75bd.h"
 #include "console.h"
+#include "logging.h"
 
 #include <FreeRTOS.h>
 #include <os_task.h>
@@ -26,63 +27,98 @@ static uint8_t thermalMgrQueueStorageArea[THERMAL_MGR_QUEUE_LENGTH * THERMAL_MGR
 
 static void thermalMgr(void *pvParameters);
 
-void initThermalSystemManager(lm75bd_config_t *config) {
+void initThermalSystemManager(lm75bd_config_t *config)
+{
   memset(&thermalMgrTaskBuffer, 0, sizeof(thermalMgrTaskBuffer));
   memset(thermalMgrTaskStack, 0, sizeof(thermalMgrTaskStack));
-  
+
   thermalMgrTaskHandle = xTaskCreateStatic(
-    thermalMgr, "thermalMgr", THERMAL_MGR_STACK_SIZE,
-    config, 1, thermalMgrTaskStack, &thermalMgrTaskBuffer);
+      thermalMgr, "thermalMgr", THERMAL_MGR_STACK_SIZE,
+      config, 1, thermalMgrTaskStack, &thermalMgrTaskBuffer);
 
   memset(&thermalMgrQueueBuffer, 0, sizeof(thermalMgrQueueBuffer));
   memset(thermalMgrQueueStorageArea, 0, sizeof(thermalMgrQueueStorageArea));
 
   thermalMgrQueueHandle = xQueueCreateStatic(
-    THERMAL_MGR_QUEUE_LENGTH, THERMAL_MGR_QUEUE_ITEM_SIZE,
-    thermalMgrQueueStorageArea, &thermalMgrQueueBuffer);
-
+      THERMAL_MGR_QUEUE_LENGTH, THERMAL_MGR_QUEUE_ITEM_SIZE,
+      thermalMgrQueueStorageArea, &thermalMgrQueueBuffer);
 }
 
-error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
+error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event)
+{
   /* Send an event to the thermal manager queue */
-  xQueueSend(thermalMgrQueueHandle, event, (TickType_t) 0);
+  error_code_t errCode;
+  if (!event)
+  {
+    return ERR_CODE_INVALID_ARG;
+  }
+  if (!thermalMgrQueueHandle)
+  {
+    return ERR_CODE_QUEUE_HANDLE_NULL;
+  }
+  if (uxQueueSpacesAvailable(thermalMgrQueueHandle) == 0)
+  {
+    return ERR_CODE_QUEUE_FULL;
+  }
+  LOG_IF_ERROR_CODE(xQueueSend(thermalMgrQueueHandle, event, (TickType_t)0));
   return ERR_CODE_SUCCESS;
 }
 
-void osHandlerLM75BD(void) { // interrupt handler
+void osHandlerLM75BD(void)
+{ // interrupt handler
   /* Implement this function */
-  thermal_mgr_event_t event = {.type = THERMAL_MGR_EVENT_TEMP_CHANGE};
+  thermal_mgr_event_t event = {.type = THERMAL_MGR_EVENT_OVER_TEMP};
   thermalMgrSendEvent(&event);
 }
 
-static void thermalMgr(void *pvParameters) {
+static void thermalMgr(void *pvParameters)
+{
   /* Implement this task */
-  thermal_mgr_event_t *const buffer;
+  thermal_mgr_event_t buffer;
   float temp;
-  while (1) {
-    if (xQueueReceive(thermalMgrQueueHandle, buffer, (TickType_t) 0) == pdTRUE){
-      readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp);
-      if (buffer->type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD){
-        addTemperatureTelemetry(temp);
-      } else if (buffer->type == THERMAL_MGR_EVENT_TEMP_CHANGE){
-        if (temp > THERMAL_MGR_HYSTERESIS_TEMP){
-          overTemperatureDetected();
-        } else {
-          safeOperatingConditions();
-        }
+  error_code_t errCode;
+  while (1)
+  {
+    if (xQueueReceive(thermalMgrQueueHandle, &buffer, (TickType_t)0) == pdFALSE)
+    {
+      LOG_ERROR(ERR_CODE_INVALID_QUEUE_MSG);
+      continue;
+    }
+    if (buffer.type == THERMAL_MGR_EVENT_MEASURE_TEMP_CMD)
+    {
+      LOG_IF_ERROR_CODE(readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp));
+      addTemperatureTelemetry(temp);
+    }
+    else if (buffer.type == THERMAL_MGR_EVENT_OVER_TEMP)
+    {
+      LOG_IF_ERROR_CODE(readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temp));
+      if (temp > THERMAL_MGR_HYSTERESIS_TEMP)
+      {
+        overTemperatureDetected();
       }
+      else
+      {
+        safeOperatingConditions();
+      }
+    }
+    else
+    {
+      LOG_ERROR(ERR_CDOE_INVALID_EVENT);
     }
   }
 }
 
-void addTemperatureTelemetry(float tempC) {
+void addTemperatureTelemetry(float tempC)
+{
   printConsole("Temperature telemetry: %f deg C\n", tempC);
 }
 
-void overTemperatureDetected(void) {
+void overTemperatureDetected(void)
+{
   printConsole("Over temperature detected!\n");
 }
 
-void safeOperatingConditions(void) { 
+void safeOperatingConditions(void)
+{
   printConsole("Returned to safe operating conditions!\n");
 }
--- services/thermal_mgr/thermal_mgr.h
+++ services/thermal_mgr/thermal_mgr.h
@@ -5,7 +5,7 @@
 
 typedef enum {
   THERMAL_MGR_EVENT_MEASURE_TEMP_CMD,
-  THERMAL_MGR_EVENT_TEMP_CHANGE
+  THERMAL_MGR_EVENT_OVER_TEMP
 } thermal_mgr_event_type_t;
 
 typedef struct {
